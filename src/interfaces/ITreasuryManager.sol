479f9d6fdd8dfdd2e0281288ee706d9e3206343a
Isla-Labs <admin@islalabs.co>
2025-10-16 20:55:28 -0300

    Remove ITreasuryManager.sol

--- src/interfaces/ITreasuryManager.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.24;
-
-interface ITreasuryManager {
-    function getTreasuries() external view returns (address platform, address rewards);
-    function getSplitBps() external view returns (uint256 rewardsBps, uint256 platformBps);
-}
