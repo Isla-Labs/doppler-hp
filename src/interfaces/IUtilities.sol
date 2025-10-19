04dc51cadf0bf6f28237deb79f67d7c054faa9f2
Isla-Labs <admin@islalabs.co>
2025-10-18 22:39:24 -0300

    Remove redundant IERC20

--- src/interfaces/IUtilities.sol
+++ src/interfaces/IUtilities.sol
@@ -5,14 +5,6 @@ import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { Currency } from "@v4-core/types/Currency.sol";
 import { IHooks } from "@v4-core/interfaces/IHooks.sol";
 
-interface IERC20 {
-    function balanceOf(address) external view returns (uint256);
-    function allowance(address, address) external view returns (uint256);
-    function approve(address, uint256) external returns (bool);
-    function transfer(address,uint256) external returns (bool);
-    function transferFrom(address,address,uint256) external returns (bool);
-}
-
 interface IWETH {
     function deposit() external payable;
     function withdraw(uint256) external;
