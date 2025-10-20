292ce9d02a706155219ac039dae6e7ba71d4570a
Isla-Labs <admin@islalabs.co>
2025-10-20 20:03:19 -0300

    Improve annotation

--- src/UniswapV4MigratorHook.sol
+++ src/UniswapV4MigratorHook.sol
@@ -20,6 +20,7 @@ import { MultiHopContext } from "src/stores/MultiHopContext.sol";
 /**
  * @title Uniswap V4 Migrator Hook with Dynamic Fees and Limit Order Support
  * @dev Extends OpenZeppelin's LimitOrderHook (https://github.com/OpenZeppelin/uniswap-hooks/blob/14be1504717139e10be4ec9c9ec95f5ffe8fba33/src/general/LimitOrderHook.sol)
+ * @dev Uses trusted forwarder pattern to replace msg.sender in OZ LimitOrderHook with new sender param; all other functionality remains intact
  * @author Whetstone Research; Isla Labs
  * @custom:security-contact security@islalabs.co
  */
