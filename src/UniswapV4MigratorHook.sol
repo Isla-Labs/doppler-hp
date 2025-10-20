efdbdbdf6c6e6b6248dd10c206439e459612191e
Isla-Labs <admin@islalabs.co>
2025-10-20 10:05:12 -0300

    Include version in OZ link

--- src/UniswapV4MigratorHook.sol
+++ src/UniswapV4MigratorHook.sol
@@ -19,7 +19,7 @@ import {MultiHopContext} from "src/stores/MultiHopContext.sol";
 
 /**
  * @title Uniswap V4 Migrator Hook with Dynamic Fees and Limit Order Support
- * @dev Extends OpenZeppelin's LimitOrderHook (https://github.com/OpenZeppelin/uniswap-hooks/blob/master/src/general/LimitOrderHook.sol)
+ * @dev Extends OpenZeppelin's LimitOrderHook (https://github.com/OpenZeppelin/uniswap-hooks/blob/14be1504717139e10be4ec9c9ec95f5ffe8fba33/src/general/LimitOrderHook.sol)
  * @author Whetstone Research, Isla Labs
  * @custom:security-contact security@islalabs.co
  */
