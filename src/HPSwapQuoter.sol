bbb3805fce94f0bdeb250b363e054f05860a3353
Isla-Labs <admin@islalabs.co>
2025-10-22 12:40:30 -0300

    Improve annotation

--- src/HPSwapQuoter.sol
+++ src/HPSwapQuoter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-// Core
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { Currency } from "@v4-core/types/Currency.sol";
